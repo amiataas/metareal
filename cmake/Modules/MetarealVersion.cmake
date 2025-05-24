fbb95efae05ed75a9167a33c69e3dbc0d17217bb
Amias Iastus <amiataas@gmail.com>
2025-05-24 14:07:15 +0330

    add: project skeleton

---
+++ cmake/Modules/MetarealVersion.cmake
@@ -0,0 +1,14 @@
+# Metareal version number information
+
+if(NOT DEFINED MR_VERSION_MAJOR)
+	set(MR_VERSION_MAJOR 1)
+endif()
+
+
+if(NOT DEFINED MR_VERSION_MINOR)
+	set(MR_VERSION_MINOR 0)
+endif()
+
+if(NOT DEFINED MR_VERSION_PATCH)
+	set(MR_VERSION_PATCH 0)
+endif()
